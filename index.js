ea4beee47e9d7e287f2b023ce6c936d798b4e549
chris-wamae <chriswamae123@gmail.com>
2023-01-05 09:06:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Dog{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+   speak = () => `${this.name} says Woof!`
+}
+
+class Cat{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak = () => `${this.name} says Meow!`
+}
+
+class Bird{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+  speak = () => {
+    if(this.sex ==="male"){
+        return `It's me!${this.name} the parrot!`
+    }
+    else{
+        return `${this.name} says squack!`
+    }
+  }
+}
